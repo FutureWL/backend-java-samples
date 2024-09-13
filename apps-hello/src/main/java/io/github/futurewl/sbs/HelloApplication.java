a54a7ee9fd41bd0cae92dd35b773fcf1d59ef13e
Future. 魏来 <624263934@qq.com>
2024-09-14 01:42:16 +0800

    更新：基于 Spring Boot 3.3.3

--- apps-hello/src/main/java/io/github/futurewl/sbs/HelloApplication.java
+++ apps-hello/src/main/java/io/github/futurewl/sbs/HelloApplication.java
@@ -1,22 +1,23 @@
-package io.github.futurewl.sbs.v31x;
+package io.github.futurewl.sbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class HelloApplicationV31x {
+public class HelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloApplicationV31x.class, args);
+        SpringApplication.run(HelloApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello(String name) {
+    @GetMapping("/name")
+    public String hello( String name) {
         return "Hello," + name;
     }
-
 
 }
